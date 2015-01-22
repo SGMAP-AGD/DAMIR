0df251e5f246592ea1b265abf6328eb7cb9e4265
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2015-01-22 20:13:57 +0100

    improve test_read

--- Fichiers_R/test_read.py
+++ Fichiers_R/test_read.py
@@ -8,10 +8,12 @@ Created on Tue Jan 20 15:36:52 2015
 import os
 import pandas as pd
 
-path = 'D:\data\health\Damir\Fichier R'
+path = 'D:/data/health/Damir/Fichier R/remove_lab'
 
 # TODO: faire bien mieux sur les dates.
 for year in range(2010, 2015):
     for month in ['0' + str(x) for x in range(1,10)] + ['10', '11', '12']:
-        file_name = os.path.join(path, 'R' + str(year) + month + '.csv')
-        print pd.read_csv(file_name, sep=';', nrows=10)
+        file_name = os.path.join(path, 'R' + str(year) + month + '.CSV')
+        table = pd.read_csv(file_name, sep=';')
+        print year, month, table[['rec_mon', 'dep_mon', 'rem_mon', 'act_dnb']].sum()/1e6
+        print (table['rec_mon'].sum()/table['act_dnb'].sum())

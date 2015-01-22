8f1af1cf8b40858e6eb2b1e60fd93516a47e4f7c
Leo <debian@debian>
2015-01-22 12:18:39 +0100

    ajout de read_me descriptif des variables

--- fichiers_supplementaires/demographie/extract_categories_prof.py
+++ fichiers_supplementaires/demographie/extract_categories_prof.py
@@ -40,6 +40,17 @@ print 'Ecriture des nouvelles tables...'
 table_situation = grouped_table.iloc[:, [0] + range(17,22)]
 file_to_write = join(write_path, return_csv_name_situ_adm)
 table_situation.to_csv(file_to_write, sep = csv_sep, index = False)
+#Read-me : Descriptif des variables
+f = open(join(write_path, return_csv_name_situ_adm[:-4] + '_readme.txt'), 'w')
+list_strings = ['P11_POP_FR : Pop Français en 2011 (princ)\n', 
+                'P11_POP_ETR : Pop Etrangers en 2011 (princ)\n',
+                'P11_POP_IMM : Pop Immigrés en 2011 (princ) \n',
+                'P11_PMEN : Pop ménages en 2011 (princ)\n',
+                'P11_PHORMEN : Pop hors ménages en 2011 (princ) \n']
+
+to_write = ''.join(list_strings)
+f.write(to_write)
+f.close()
 print 'Table des situations administratives ecrite (1/2)'
 
 #Table de l'emploi
@@ -53,7 +64,22 @@ table_emploi.drop('variable', axis = 1, inplace = True)
 columns = [table_emploi.columns[0]] + list(table_emploi.columns[2:]) + [table_emploi.columns[1]]
 table_emploi = table_emploi[columns]
 table_emploi.sort('DEP', inplace = True)
+file_to_write = join(write_path, return_csv_name_emploi)
 table_emploi.to_csv(file_to_write, sep = csv_sep, index = False)
+#Read-me : Descriptif des variables
+f = open(join(write_path, return_csv_name_emploi[:-4] + '_readme.txt'), 'w')
+list_strings = ['value : population de 15 ans ou plus en 2011 \n',
+                'CS1 : Agriculteurs exploitants\n',
+                'CS2 : Artisans, Comm., Chefs entr.\n',
+                'CS3 : Cadres, Prof. intel. sup.\n',
+                'CS4 : Prof. intermédiaires\n',
+                'CS5 : Employés\n',
+                'CS6 : Ouvriers\n',
+                'CS7 : Retraités\n',
+                'CS8 : Autres\n']
+to_write = ''.join(list_strings)
+f.write(to_write)
+f.close()
 print 'Table des emplois ecrite (2/2)'
 print 'FIN'
 ########### Fin du script
